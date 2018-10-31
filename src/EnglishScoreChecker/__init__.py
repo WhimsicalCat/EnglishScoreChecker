f122a36a57c207dfe847c53bc8f5f2c3a6a186a5
Takimoto Hiroki <whimsical.cat.cat@gmail.com>
2018-10-31 11:18:58 +0900

    MODIFY:Flaskの初期化とブループリントの追加を行う処理を実装した

--- src/EnglishScoreChecker/__init__.py
+++ src/EnglishScoreChecker/__init__.py
@@ -5,3 +5,13 @@ Created on 2018/10/29
 '''
 
 from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    
+    from .view import blueprint_esc
+    app.register_blueprint(blueprint_esc)
+    
+    return app
+
+app = create_app()
