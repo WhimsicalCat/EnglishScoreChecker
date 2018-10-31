2149511552f1629f00c5fc2fdda6f59a63eaffc6
Takimoto Hiroki <whimsical.cat.cat@gmail.com>
2018-10-31 15:54:01 +0900

    ADD:configスクリプトを追加した

---
+++ src/config.py
@@ -0,0 +1,16 @@
+'''
+Created on 2018/03/28
+
+@author: Takimoto Hiroki
+'''
+
+import os
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+
+class DevelopConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    
