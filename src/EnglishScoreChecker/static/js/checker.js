05b2fa693d8c47f1708d9c521cdea3c3c3ffc5d4
Takimoto Hiroki <whimsical.cat.cat@gmail.com>
2018-11-05 11:39:29 +0900

    MODIFY:不要なタグを削った

--- src/EnglishScoreChecker/static/js/checker.js
+++ src/EnglishScoreChecker/static/js/checker.js
@@ -1,17 +1,16 @@
-//$('#selector a').on('click', function (e) {
-//  e.preventDefault()
-//  $(this).tab('show')
-//})
+var activeTab = "#essay";
 
 $(function() {
   $('a[data-toggle="tab"]').on('click', function(e) {
 //        window.localStorage.setItem('activeTab', $(e.target).attr('href'));
-    var activeTab = $(e.target).attr('href');
+    activeTab = $(e.target).attr('href');
+    console.log(activeTab);
     $('#selector a[href="' + activeTab + '"]').tab('show');
   });
+  console.log(activeTab);
 //  var activeTab = window.localStorage.getItem('activeTab');
 //  if (activeTab) {
 //    $('#myTab a[href="' + activeTab + '"]').tab('show');
 //    window.localStorage.removeItem("activeTab");
-  }
+//  }
 });
