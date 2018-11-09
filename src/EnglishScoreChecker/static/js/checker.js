8e6e3ca775f2e8fa3c49e615431c23eb216558b2
Tom Holley <t.h.felis.silvestris@gmail.com>
2018-11-09 14:47:58 +0900

    REMOVE:使用しなくなったコード類を削除した

--- src/EnglishScoreChecker/static/js/checker.js
+++ src/EnglishScoreChecker/static/js/checker.js
@@ -1,13 +1,8 @@
-//$('#selector a').on('click', function (e) {
-//  e.preventDefault()
-//  $(this).tab('show')
-//})
 
 $(function() {
   var activeTab = $('a[data-toggle="tab"][class*="active"]').attr('href');
   $('a[data-toggle="tab"]').on('click', function(e) {
     activeTab = $(e.target).attr('href');
-//    $('#selector a[href="' + activeTab + '"]').tab('show');
     console.log(activeTab);
   });
   $('textarea[id="inputtext"').keyup(function(e) {
