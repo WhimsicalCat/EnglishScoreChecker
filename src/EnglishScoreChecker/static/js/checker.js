5c51c2a9ac43f9916a2361ff21a3a0b525475d50
Tom Holley <t.h.felis.silvestris@gmail.com>
2018-11-09 15:48:24 +0900

    MODIFY:テキストボックスの更新に応じて文字数を更新する処理を実装した

--- src/EnglishScoreChecker/static/js/checker.js
+++ src/EnglishScoreChecker/static/js/checker.js
@@ -3,10 +3,23 @@ $(function() {
   var activeTab = $('a[data-toggle="tab"][class*="active"]').attr('href');
   $('a[data-toggle="tab"]').on('click', function(e) {
     activeTab = $(e.target).attr('href');
-    console.log(activeTab);
+//    console.log(activeTab);
   });
-  $('textarea[id="inputtext"').keyup(function(e) {
-    console.log($(e.target).val())
+  $('textarea[id="inputtext"]').keyup(function(e) {
+//    console.log($(e.target).val());
+    $.ajax({
+      url: 'count_word',
+      type: "POST",
+      data: {
+        'data':$(e.target).val()
+      }
+    })
+    .done(function(recv){
+      $('#num_of_word').text(recv)
+    })
+    .fail(function(recv){
+      
+    });
   });
-  console.log(activeTab);
+//  console.log(activeTab);
 });
--- src/EnglishScoreChecker/view.py
+++ src/EnglishScoreChecker/view.py
@@ -14,3 +14,8 @@ blueprint_esc = Blueprint(DEF_blp_name, __name__)
 def index():
     return flask.render_template('checker_page.html')
 
+@blueprint_esc.route('/count_word', methods=['POST'])
+def count_num_of_words():
+    text_data = flask.request.form['data']
+    print(text_data)
+    return '{}'.format(len(text_data.strip('.').split()))
